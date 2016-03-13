9340c414a24ca734c34e65622a0cdac1c2591fd6
Guranant Singh Virdee <anantvirdee@gmail.com>
2016-03-13 17:05:19 -0500

    Users added using scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text:  'Hello Anant'
+    render text:  "&igrave Hello Anant"
   end
 end
